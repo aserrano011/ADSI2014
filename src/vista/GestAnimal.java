14a60c953ec04554ecd5469132460a2a197bf338
MAGNUM <MAGNUM@192.168.1.129>
2015-01-10 04:05:13 +0100

    Intento3...

--- src/vista/GestAnimal.java
+++ src/vista/GestAnimal.java
@@ -5,14 +5,32 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import net.miginfocom.swing.MigLayout;
 import javax.swing.JButton;
 import javax.swing.JList;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JScrollPane;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JSplitPane;
+import javax.swing.ListSelectionModel;
+
+import modelo.GestorAnimales;
+import modelo.GestorBD;
 
 public class GestAnimal extends JFrame {
 
 	/**
-	 * 
+	 *@author Endika Serrano Lomas
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -39,26 +57,73 @@ public class GestAnimal extends JFrame {
 	public GestAnimal() {
 		setTitle("Gestionar animales");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 312, 300);
+		setBounds(100, 100, 386, 382);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[grow,center][grow,center]", "[grow][][][][][][][]"));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JList lstAnimales = new JList();
-		contentPane.add(lstAnimales, "cell 0 0 2 6,grow");
+		//BOTONES
+		
+		JPanel panelBotones = new JPanel();
+		contentPane.add(panelBotones, BorderLayout.SOUTH);
 		
 		JButton btnAnadir = new JButton("A\u00F1adir");
-		contentPane.add(btnAnadir, "cell 0 6");
+		panelBotones.add(btnAnadir);
 		
 		JButton btnModificar = new JButton("Modificar");
-		contentPane.add(btnModificar, "cell 1 6");
+		panelBotones.add(btnModificar);
 		
 		JButton btnEliminar = new JButton("Eliminar");
-		contentPane.add(btnEliminar, "cell 0 7");
+		panelBotones.add(btnEliminar);
 		
 		JButton btnAtras = new JButton("Atr\u00E1s");
-		contentPane.add(btnAtras, "cell 1 7");
+		btnAtras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnAtras.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent arg0) {
+				dispose();
+			}
+		});
+		panelBotones.add(btnAtras);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JLabel lblToros = new JLabel("Toros");
+		lblToros.setHorizontalAlignment(SwingConstants.LEFT);
+		panel.add(lblToros);
+		
+		JLabel lblCabestros = new JLabel("Cabestros");
+		panel.add(lblCabestros);
+		
+		
+		//LISTAS
+		JScrollPane scrollPaneT = new JScrollPane();
+		JScrollPane scrollPaneC = new JScrollPane();
+
+		JList listaToros = new JList();
+		listaToros.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JList listaCabestros = new JList();
+		listaCabestros.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		JSplitPane splitPane = new JSplitPane();
+		contentPane.add(splitPane, BorderLayout.CENTER);
+		splitPane.setLeftComponent(scrollPaneT);
+		splitPane.setRightComponent(scrollPaneC);
+		
+		scrollPaneT.setViewportView(listaToros);
+		scrollPaneC.setViewportView(listaCabestros);
+		
+		splitPane.setDividerLocation(170);
 	}
+	
+	public void cargarDatosListas(){
+		GestorAnimales.getInstance().obtenerToros(Principal.getIdGanaderia());
+		GestorAnimales.getInstance().obtenerCabestros(Principal.getIdGanaderia());
+	}
+	
 
 }
